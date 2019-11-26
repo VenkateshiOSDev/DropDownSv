198b12ec49f50d7db387c87448032e58f14705f3
venkatesh s <venkatesu.savarala@krafty.in>
2019-11-26 09:32:52 +0530

    Initial project setup

---
+++ Source/DropDownSv.swift
@@ -0,0 +1,13 @@
+//
+//  DropDownSv.swift
+//  DropDownSv
+//
+//  Created by Venkatesh S on 26/11/2019.
+//  Copyright © 2019 krafty. All rights reserved.
+//
+
+import Foundation
+public class DropDownSv {
+  static let shared = DropDownSv()
+  func foo() {}
+}
